0df99582ba876faf1d4137271b2c0784f3356af4
ashrafomid2025 <omidmashraf@gmail.com>
2025-07-27 19:10:24 +0430

    final touch

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,7 @@
 export default function Header() {
   return (
     <div className="w-full p-4 text-center bg-purple-500 text-4xl text-white mb-4">
-      <h1>TODO APP</h1>
+      <h1>TODO APPLICATION</h1>
     </div>
   );
 }
